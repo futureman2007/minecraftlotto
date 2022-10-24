4609223db559cdea3385621172d4f22e282a5d6d
Futureman2007 <penner.stephan@gmx.de>
2022-10-24 23:23:16 +0200

    removed Debug

--- helper/minecraftScreenHelper.py
+++ helper/minecraftScreenHelper.py
@@ -3,11 +3,9 @@ from helper import screenHelper
 
 def send_say_in_minecraft_session(stringToSay):
     if (not stringToSay):
-        print("Debug: String to say in minecraft is not right!")
         return None
 
     if (screenHelper.is_session_present("minecraft") == False):
-        print("Debug: is_session_present in minecraft is not right!")
         return None
     #screen -s minecraft -X stuff $'say "***Starting Backup...\n'
     #ret = os.popen("screen -S minecraft -X stuff \"say ***Starting Backup\"" + "^M")
