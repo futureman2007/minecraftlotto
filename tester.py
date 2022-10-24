51390ad155be327e3bb9421a1f4aedac359034e1
Futureman2007 <penner.stephan@gmx.de>
2022-10-24 22:10:50 +0200

    errorfix

--- tester.py
+++ tester.py
@@ -4,7 +4,7 @@ from helper import screenHelper
 print("**** DEBUG AND TESTING ****")
 
 print("***screenHelper.get_running_screen_sessions()")
-print(screenHelper.get_running_screen_sessions)
+print(screenHelper.get_running_screen_sessions())
 
 print("***screenHelper.is_session_present")
 print(screenHelper.is_session_present("minecraft"))
