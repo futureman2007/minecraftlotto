4b5e31cace13651b0980c856c8ede622d54ef8cc
Futureman2007 <penner.stephan@gmx.de>
2022-10-25 23:17:46 +0200

    bugfix in methodcall

--- tester.py
+++ tester.py
@@ -27,18 +27,18 @@ print("***fileManager.clean_file_content_relative_path(testfiles, testfile1.txt)
 print(fileManager.clean_file_content_relative_path("testfiles", "testfile1.txt"))
 
 print("***fileManager.clean_file_content_relative_path(path=/home/minecraft/scripts/python/minecraftlotto/testfiles/,filename=testfile3.txt, destpath=/home/minecraft/scripts/python/minecraftlotto/testfiles/, destfilename=copy_existing_file_test.txt)")
-print(fileManager.copy_existing_file(path="/home/minecraft/scripts/python/minecraftlotto/testfiles/", 
+print(fileManager.copy_existing_file(path="/home/minecraft/scripts/python/minecraftlotto/testfiles", 
                                     filename="testfile3.txt",
                                     destpath="/home/minecraft/scripts/python/minecraftlotto/testfiles/", 
                                     destfilename="copy_existing_file_test.txt"))
 
 print("***fileManager.copy_existing_file_and_clean_srcfile()")
-print(fileManager.copy_existing_file_and_clean_srcfile(path="/home/minecraft/scripts/python/minecraftlotto/testfiles/", 
+print(fileManager.copy_existing_file_and_clean_srcfile(path="/home/minecraft/scripts/python/minecraftlotto/testfiles", 
                                     filename="testfile4.txt",
                                     destpath="/home/minecraft/scripts/python/minecraftlotto/testfiles/", 
                                     destfilename="copy_existing_file_and_clean_srcfile.txt"))
 
-print(fileManager.append_content_to_file(srcpath="/home/minecraft/scripts/python/minecraftlotto/testfiles/", 
+print(fileManager.append_content_to_file(srcpath="/home/minecraft/scripts/python/minecraftlotto/testfiles", 
                                     srcfilename="testfile5.txt",
                                     content="[THIS SHOULD BE NOW ADDED]" + "\n" + "[AND THIS ALSO BUT IN A NEW LINE]"))
 
