e98444ea5774ad62a9c5c990d7f882783d1d2c72
Futureman2007 <penner.stephan@gmx.de>
2022-10-25 12:53:16 +0200

    added tester and new functionality

--- tester.py
+++ tester.py
@@ -1,5 +1,6 @@
 from helper import screenHelper
 from helper import minecraftScreenHelper
+from helper import fileManager
 
 print("**** DEBUG AND TESTING ****")
 
@@ -14,4 +15,9 @@ print(screenHelper.is_session_present("none"))
 
 print("***minecraftScreenHelper.send_say_in_minecraft_session(I sended this using Python :D)")
 print(minecraftScreenHelper.send_say_in_minecraft_session("I sended this using Python :D"))
+
+print("***")
+print(fileManager.clean_file_content_abs_path("/home/minecraft/testfile.txt"))
+
+print(copy_existing_file)
 
